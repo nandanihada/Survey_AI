d191030aaf38432549c2621ab1a222d4f3a39595
Nandani Hada <157029829+nandanihada@users.noreply.github.com>
2025-06-30 12:12:34 +0530

    real is blank_SurveyAI

--- mongodb_config.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["pepper_database"]  # Use your actual DB name
