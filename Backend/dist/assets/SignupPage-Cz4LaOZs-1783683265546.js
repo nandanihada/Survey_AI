ee9af95e2889c334e2e8d75fa423a5509cc2e856
Nandani Hada <157029829+nandanihada@users.noreply.github.com>
2026-07-13 17:14:42 +0530

    update: sync frontend build to Backend/dist for deployment

--- Backend/dist/assets/SignupPage-Cz4LaOZs-1783683265546.js
+++
@@ -1,1 +0,0 @@
-import{e as t,r as a}from"./index-DfXs1o3s-1783683265546.js";function n(){const e=t();return a.useEffect(()=>{e("/login?mode=signup",{replace:!0})},[e]),null}export{n as default};
---
+++ Backend/dist/assets/SignupPage-hMJ3qlWE-1783942087431.js
@@ -0,0 +1,1 @@
+import{e as t,r as a}from"./index-D363Ylp6-1783942087431.js";function n(){const e=t();return a.useEffect(()=>{e("/login?mode=signup",{replace:!0})},[e]),null}export{n as default};
